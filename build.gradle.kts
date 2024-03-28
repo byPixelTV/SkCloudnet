5aecdafc43355af99a50cfd1be557c565ffbb17a
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-03-28 16:02:02 +0100

    Finaly Skript registers the Addon, now I need to implement some basic Skript syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,15 @@ plugins {
     kotlin("jvm") version "1.9.23"
     id("io.papermc.paperweight.userdev") version "1.5.12"
     id("xyz.jpenilla.run-paper") version "1.1.0"
-    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
 group = "de.bypixeltv"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
+    maven(url = "https://s01.oss.sonatype.org/content/repositories/snapshots/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     maven {
         name = "papermc"
@@ -26,8 +26,32 @@ dependencies {
     implementation("dev.jorel", "commandapi-bukkit-shade", "9.3.0")
     implementation("dev.jorel", "commandapi-bukkit-kotlin", "9.3.0")
     implementation("net.axay:kspigot:1.20.3")
+
     implementation("com.github.SkriptLang:Skript:2.8.3")
-    library(kotlin("stdlib"))
+
+
+    implementation("eu.cloudnetservice.cloudnet:syncproxy:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:bridge:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:driver:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:platform-inject-runtime:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:platform-inject-processor:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:platform-inject-loader:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:platform-inject-api:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:platform-inject-support:4.0.0-RC9")
+    implementation("eu.cloudnetservice.cloudnet:common:4.0.0-RC9")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
 }
 
 kotlin {
--- src/main/kotlin/de/bypixeltv/skcloudnet/Main.kt
+++ src/main/kotlin/de/bypixeltv/skcloudnet/Main.kt
@@ -1,5 +1,42 @@
-package de.bypixeltv.de.bypixeltv.skcloudnet
+package de.bypixeltv.skcloudnet
 
-fun main() {
-    println("Hello World!")
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
+import net.kyori.adventure.text.minimessage.MiniMessage
+import ch.njol.skript.Skript
+import ch.njol.skript.SkriptAddon
+import org.bukkit.plugin.java.JavaPlugin
+import java.io.IOException
+
+class Main : JavaPlugin() {
+
+    private val miniMessages = MiniMessage.miniMessage()
+
+    lateinit var instance: Main
+    lateinit var addon: SkriptAddon
+
+    @Suppress("Deprecation")
+    override fun onEnable() {
+        instance = this
+        addon = Skript.registerAddon(this)
+        server.consoleSender.sendMessage(miniMessages.deserialize("<blue>Successfully enabled SkCloudnet v1! <yellow>Good to see you :)</yellow></blue>"))
+    }
+
+    override fun onLoad() {
+        server.consoleSender.sendMessage(miniMessages.deserialize("<blue>Loading SkCloudnet...</blue>"))
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).silentLogs(true).verboseOutput(true))
+    }
+
+    override fun onDisable() {
+        CommandAPI.onDisable()
+        server.consoleSender.sendMessage(miniMessages.deserialize("<blue>Successfully disabled SkCloudnet v1! <yellow>Goodbye!</yellow></blue>"))
+    }
+
+    fun getMainInstance(): Main {
+        return instance
+    }
+
+    fun getAddonInstance(): SkriptAddon {
+        return addon
+    }
 }
