9dbba1739d9930d128ec55523f6fb04a4a8fd9ba
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-04-12 19:44:02 +0200

    Added EffStartService (idk why this wasn't already added because there is EffStartAllServices and EffStartAllServicesOnTask xD)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "de.bypixeltv"
-version = "1.4"
+version = "1.5"
 
 repositories {
     mavenCentral()
---
+++ src/main/kotlin/de/bypixeltv/skcloudnet/elements/effects/services/EffStartService.kt
@@ -0,0 +1,54 @@
+package de.bypixeltv.skcloudnet.elements.effects.services
+
+import ch.njol.skript.Skript
+import ch.njol.skript.doc.Description
+import ch.njol.skript.doc.Examples
+import ch.njol.skript.doc.Name
+import ch.njol.skript.doc.Since
+import ch.njol.skript.lang.Effect
+import ch.njol.skript.lang.Expression
+import ch.njol.skript.lang.SkriptParser
+import ch.njol.util.Kleenean
+import eu.cloudnetservice.driver.inject.InjectionLayer
+import eu.cloudnetservice.driver.provider.CloudServiceProvider
+import org.bukkit.event.Event
+
+@Name("Start Service")
+@Description("Start a CloudNet service by its name.")
+@Examples("start cloudnet service \"Lobby-1\"")
+@Since("1.0")
+
+class EffStartService : Effect() {
+
+    private val cnServiceProvider: CloudServiceProvider = InjectionLayer.ext().instance(CloudServiceProvider::class.java)
+
+    companion object{
+        init {
+            Skript.registerEffect(EffStartService::class.java, "start [cloudnet] service %string%")
+        }
+    }
+
+    private var serviceExpression: Expression<String>? = null
+
+    @Suppress("UNCHECKED_CAST")
+    override fun init(
+        expressions: Array<Expression<*>>,
+        matchedPattern: Int,
+        isDelayed: Kleenean,
+        parser: SkriptParser.ParseResult
+    ): Boolean {
+        this.serviceExpression = expressions[0] as Expression<String>
+        return true
+    }
+
+    override fun toString(event: Event?, debug: Boolean): String {
+        return "start cloudnet service ${serviceExpression?.getSingle(event)}"
+    }
+
+    override fun execute(event: Event?) {
+        val service = serviceExpression?.getSingle(event)
+        service?.let {
+            cnServiceProvider.serviceProviderByName(it).startAsync()
+        }
+    }
+}
