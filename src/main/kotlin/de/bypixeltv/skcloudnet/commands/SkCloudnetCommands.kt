d8e9e8a73d22458fa9333d759761c1134661997d
Lukas <113579672+byPixelTV@users.noreply.github.com>
2024-05-01 00:26:49 +0200

    A bug in EffKickPlayerFromCloud made it impossible to kick a player without a reason, this is now fixed. Also readded the fixed version of EffPlayerSendCloudMessage and fixed other bugs. Change the version to 1.7.0 and updated the update checker

--- src/main/kotlin/de/bypixeltv/skcloudnet/commands/SkCloudnetCommands.kt
+++ src/main/kotlin/de/bypixeltv/skcloudnet/commands/SkCloudnetCommands.kt
@@ -13,6 +13,7 @@ import java.nio.file.Paths
 class SkCloudnetCommands {
     private val miniMessages = MiniMessage.miniMessage()
 
+    @Suppress("UNUSED", "DEPRECATION")
     val command = commandTree("skcloudnet") {
         withPermission("skcloudnet.admin")
         literalArgument("info") {
@@ -48,16 +49,16 @@ class SkCloudnetCommands {
         literalArgument("version") {
             withPermission("skcloudnet.admin.version")
             playerExecutor { player, _ ->
-                val githubVersion = VersionUtils().getLatestAddonVersion()?.replace("v", "")?.toDouble()
+                val githubVersion = VersionUtils().getLatestAddonVersion()?.replace("v", "")
+                val currentVersion = Main.INSTANCE.description.version.replace("v", "")
+
                 if (githubVersion != null) {
-                    if (githubVersion > Main.INSTANCE.description.version.replace("v", "").toDouble()) {
-                        player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#43fa00>There is an update available for SkCloudnet!</color> <aqua>You're on version <yellow>${Main.INSTANCE.description.version}</yellow> and the latest version is <yellow>$githubVersion</yellow></aqua>!\n\n<color:#43fa00>Download the latest version here:</color> <blue>https://github.com/byPixelTV/SkCloudnet/releases</blue> <aqua>"))
+                    if (VersionUtils().isVersionGreater(githubVersion, currentVersion)) {
+                        player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#43fa00>There is an update available for SkCloudnet!</color> <aqua>You're on version <yellow>${Main.INSTANCE.description.version}</yellow> and the latest version is <yellow>$githubVersion</yellow></aqua>!<color:#43fa00>Download the latest version here:</color> <blue>https://github.com/byPixelTV/SkCloudnet/releases</blue> <aqua>"))
+                    } else if (githubVersion == currentVersion) {
+                        return@playerExecutor
                     } else {
-                        if (githubVersion == Main.INSTANCE.description.version.replace("v", "").toDouble()) {
-                            player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#43fa00>You're on the latest version of SkCloudnet!</color> <aqua>Version <yellow>${Main.INSTANCE.description.version}</yellow></aqua>"))
-                        } else if (githubVersion < Main.INSTANCE.description.version.replace("v", "").toDouble()) {
-                            player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#ff0000>You're running a development version of SkCloudnet! Please note that this version may contain bugs!</color> <aqua>Version <color:#ff0000>${Main.INSTANCE.description.version}</color> > <color:#43fa00>${VersionUtils().getLatestAddonVersion()}</color></aqua>"))
-                        }
+                        player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#ff0000>You're running a development version of SkCloudnet! Please note that this version may contain bugs!</color> <aqua>Version <color:#ff0000>${Main.INSTANCE.description.version}</color> > <color:#43fa00>${VersionUtils().getLatestAddonVersion()}</color></aqua>"))
                     }
                 } else {
                     player.sendMessage(miniMessages.deserialize("<dark_grey>[<gradient:aqua:blue:aqua>SkCloudnet</gradient>]</dark_grey> <color:#ff0000>Unable to fetch the latest version from Github!</color> <aqua>Are you rate limited?</aqua>"))
--- src/main/kotlin/de/bypixeltv/skcloudnet/elements/effects/players/EffPlayerSendCloudMessage.kt
+++ src/main/kotlin/de/bypixeltv/skcloudnet/elements/effects/players/EffPlayerSendCloudMessage.kt
@@ -23,20 +23,20 @@ import org.jetbrains.annotations.Nullable
 @Examples("kick \"byPixelTV\" parsed as player from proxy due to \"Star SkCloudnet now!!!\"")
 @Since("1.4")
 
-class EffKickPlayerFromCloud : Effect() {
+class EffPlayerSendCloudMessage : Effect() {
 
     private val serviceRegistry: ServiceRegistry = InjectionLayer.ext().instance(ServiceRegistry::class.java)
     private val playerManager: PlayerManager = serviceRegistry.firstProvider(PlayerManager::class.java)
 
     companion object{
         init {
-            Skript.registerEffect(EffKickPlayerFromCloud::class.java, "kick %players% from (cloud|cloudnet|network|proxy|bungee|velocity|bungeecord) [(by reason of|because [of]|on account of|due to|with reason) %-string%]")
+            Skript.registerEffect(EffPlayerSendCloudMessage::class.java, "send (cloud|cloudnet|bungee|velocity|proxy|bungeecord) (message|msg) %string% to [the] [player] %players%")
         }
     }
 
-    private var players: Expression<Player>? = null
+    private var message: Expression<String>? = null
     @Nullable
-    private var reason: Expression<String>? = null
+    private var players: Expression<Player>? = null
 
     @Suppress("UNCHECKED_CAST")
     override fun init(
@@ -45,22 +45,19 @@ class EffKickPlayerFromCloud : Effect() {
         isDelayed: Kleenean,
         parser: SkriptParser.ParseResult
     ): Boolean {
-        this.players = expressions[0] as Expression<Player>
-        if (expressions.size > 1 && expressions[1] != null) {
-            this.reason = expressions[1] as Expression<String>
-        }
+        this.message = expressions[0] as Expression<String>
+        this.players = expressions[1] as Expression<Player>
         return true
     }
 
     override fun toString(event: Event?, debug: Boolean): String {
-        return "kick ${this.players} from cloudnet ${this.reason}"
+        return "send cloud msg ${this.message} to player ${this.players}"
     }
 
     override fun execute(e: Event?) {
-        val r = if (reason != null) reason!!.getSingle(e) else ""
-        if (r == null) return
+        val msg = message?.getSingle(e)
         for (p in players!!.getArray(e)) {
-            playerManager.playerExecutor(p.uniqueId).kick(literalText(r))
+            msg?.let { literalText(it) }?.let { playerManager.playerExecutor(p.uniqueId).sendChatMessage(it) }
         }
     }
 }
